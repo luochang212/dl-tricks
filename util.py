f65b321018bea367db63c5bb3a7c9f322c397a86
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-07-11 03:26:25 +0800

    update

---
+++ util.py
@@ -0,0 +1,173 @@
+import json
+import numpy as np
+import pandas as pd
+import torch
+import matplotlib.pyplot as plt
+from ast import literal_eval
+from IPython import display
+from torch.utils.data import DataLoader, TensorDataset
+
+
+# 参数 ebd_cols 定义哪些列存了 embedding
+def embedding_df_to_csv(df, csv_path, ebd_cols: list):
+    """将带有 embedding 的 DataFrame 存入 csv"""
+    def ebd2str(embedding):
+        if not isinstance(embedding, list):
+            ebd = embedding.tolist()
+        return json.dumps(ebd)
+
+    for col in ebd_cols:
+        df[col] = df[col].apply(ebd2str)
+
+    df.to_csv(csv_path, index=False)
+
+
+def read_embedding_csv(csv_path, ebd_cols: list):
+    """将带有 embedding 的 csv 读入 DataFrame"""
+    df = pd.read_csv(csv_path)
+    for col in ebd_cols:
+        df[col] = df[col].apply(literal_eval).apply(lambda e: np.array(e))
+
+    return df
+
+
+def get_avg_embeddings(sentences, tokenizer, model):
+    """计算句子的平均嵌入"""
+    encoded_inputs = tokenizer(corpus, return_tensors='pt')
+    with torch.no_grad():
+        outputs = model(**encoded_inputs)
+        embeddings = outputs.last_hidden_state.mean(dim=1)
+
+    return embeddings
+
+
+class Animator:
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        self.use_svg_display()
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: self.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+        
+    @staticmethod
+    def use_svg_display():
+        """Use the svg format to display a plot in Jupyter.
+
+        Defined in :numref:`sec_calculus`"""
+        from matplotlib_inline import backend_inline
+        backend_inline.set_matplotlib_formats('svg')
+    
+    @staticmethod
+    def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+        """Set the axes for matplotlib.
+
+        Defined in :numref:`sec_calculus`"""
+        axes.set_xlabel(xlabel), axes.set_ylabel(ylabel)
+        axes.set_xscale(xscale), axes.set_yscale(yscale)
+        axes.set_xlim(xlim),     axes.set_ylim(ylim)
+        if legend:
+            axes.legend(legend)
+        axes.grid()
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+
+class Accumulator:
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def accuracy(y_hat, y):
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def evaluate_accuracy(net, data_iter):
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def gen_iter(X: np.ndarray,
+             y: np.ndarray,
+             batch_size: int,
+             num_workers: int = 4,
+             shuffle: bool = True
+    ) -> DataLoader:
+    """生成数据迭代器"""
+    assert X.dtype == np.float32
+    assert y.dtype == np.int64
+
+    X_tensor = torch.from_numpy(X)
+    y_tensor = torch.from_numpy(y)
+    dataset = TensorDataset(X_tensor, y_tensor)
+    return DataLoader(dataset,
+                      batch_size,
+                      shuffle=shuffle,
+                      num_workers=num_workers)
+
+
+def train_test_split(X, y, test_size=0.2, random_state=None):
+    """分割训练集和验证集"""
+    assert len(X) == len(y)
+
+    if random_state is not None:
+        np.random.seed(random_state)
+
+    n_samples = len(X)
+    indexes = np.random.permutation(n_samples)
+    test_len = int(n_samples * test_size)
+
+    # 计算 train / test 索引
+    train_indexes = indexes[test_len:]
+    test_indexes = indexes[:test_len]
+
+    return X[train_indexes], X[test_indexes], y[train_indexes], y[test_indexes]
